73780b2a3c7883e1a7a3027bdd0674a1ef267de9
Ajey <ajey.charantimath@gmail.com>
2016-09-03 20:13:50 +0530

    Search repo by language feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 import { Search } from './search';
 import { GithubSearchService } from './github.service';
 import { CONSTANTS } from '../shared';
+
+declare var $:any;
 
 @Component({
     selector: 'as-home',
@@ -9,60 +11,120 @@ import { CONSTANTS } from '../shared';
     styleUrls: [
         'app/home/home.css'
     ],
-    providers: [GithubSearchService]
+   directives: [  ],
+    providers: [ GithubSearchService ]
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
     public repos: Array<any> = [];
-    
+    public originalRepos: Array<any> = [];
+    public languageTags: Array<any> = [];
+
+    public elementRef: ElementRef;
     public strings;
     public model = new Search('');
     public fetching: Boolean = false;
     public isError: Boolean = false;
-    public profileImage: string = "";
     public isEmpty = null;
-
-    constructor(private githubSearchService: GithubSearchService) {
+    public repoCount = 0;
+    public pageNumber = 1;
+    public loadingMore = false;
+    public maxStarCount = 100;
+    public starSlider;
+    
+    constructor(private githubSearchService: GithubSearchService, elementRef: ElementRef) {
+        this.elementRef = elementRef;
         this.strings = CONSTANTS.MAIN.APP;
     }
 
     onSubmit() { 
         this.fetching = true;
-        this.githubSearchService.getReposByUsername(this.model.text)
+        this.clearRepos();
+        this.fetchRepos(this.pageNumber);
+    }
+
+    fetchRepos(pageNumber) {
+        this.githubSearchService.getReposByUsername(this.model.text, pageNumber)
                         .subscribe(
                             this.success.bind(this),
                             this.error.bind(this)
                         );
     }
 
+    fetchNextPage() {
+        this.loadingMore = true;
+        this.pageNumber++;
+        this.fetchRepos(this.pageNumber);
+    }
+
+    clearRepos() {
+        this.repos = [];
+        this.originalRepos = [];
+    }
+
     success(repos) {
-        this.repos = repos;
+        this.repos = this.repos.concat(repos.items);
+        this.originalRepos = this.repos;
+        this.repoCount = repos.total_count;
+        
         this.isError = false;
 
         if(repos.length === 0) {
-            this.profileImage = "";
             this.isEmpty = true;
         } else {
             this.isEmpty = false;
-            this.getImage();
+            this.getMaxStarCount();
         }
 
         this.fetching = false;
+        this.loadingMore = false;
     }
 
     error(error) {
-        console.log("errror", error);
         this.handleError();
     }
 
-    getImage() {
-        this.profileImage = this.repos[0].owner.avatar_url;
+    getMaxStarCount() {
+        this.maxStarCount = this.repos[0].stargazers_count;
+        this.starSlider.slider("option", "max", this.maxStarCount);
+        this.starSlider.slider("option", "value", this.maxStarCount);
     }
 
     handleError() {
         this.fetching = false;
         this.isError = true;
-        this.profileImage = null;
         this.isEmpty = false;
         this.repos = null;
+    }
+
+    // Slider Range Change detection
+    filterReposByStar(stars) {
+       this.repos =  this.originalRepos.filter(function(repo) {
+            return repo.stargazers_count >= stars;
+        });
+
+    }
+
+    ngAfterViewInit() {
+        // Init Autocomplete
+        this.githubSearchService.getLanguageTags().subscribe(function(tags){
+            this.languageTags = tags;
+            $(this.elementRef.nativeElement).find(".search-text").autocomplete({
+                source: this.languageTags
+            });
+        }.bind(this), function(error){
+            console.error("Could not fetch language tags", error);
+        }.bind(this));
+
+        // Initialize Slider
+        this.starSlider = $(this.elementRef.nativeElement).find("#slider").slider({
+            range: false,
+            min: 0,
+            max: 20000,
+            value: this.maxStarCount,
+            slide: ( event, ui ) => {
+                this.maxStarCount = ui.value;
+                this.filterReposByStar(ui.value);
+            }
+        });
     }
 }
--- src/app/shared/constant/main.ts
+++ src/app/shared/constant/main.ts
@@ -1,8 +1,8 @@
 export const MAIN = {
     APP: {
         BRAND: 'Angular 2 GitHub Search',
-        ERROR_MSG: 'Sorry could find the repos of the given user. Try again!',
-        EMPTY_REPOS: 'No Repos found for the user',
+        ERROR_MSG: 'Sorry could find repos. Try again!',
+        EMPTY_REPOS: 'No Repos!',
         EXPLORE_PROJECT: 'EXPLORE PROJECT'
     }
 };
